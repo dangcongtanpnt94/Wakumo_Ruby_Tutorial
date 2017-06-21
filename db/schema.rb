bc4de0840cde67be85b09e710e9f800ab35cb761
congtan <dangcongtanpnt94@gmail.com>
2017-06-21 11:48:27 +0700

    change to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170621021124) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "members", force: :cascade do |t|
     t.string "name"
     t.integer "gender"
